23a94d4d111cfc850d95843429e0b2c210d25f54
unknown <azizovagultakin@gmail.com>
2023-10-01 18:38:03 +0300

    scrol and hamburgermenu

--- frontend/src/layout/Site/Header/Header.jsx
+++ frontend/src/layout/Site/Header/Header.jsx
@@ -4,40 +4,66 @@ import PersonIcon from '@mui/icons-material/Person';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Menu'
 import '../Header/Header.scss';
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-    <div className="navbar">
-      <div className="navbar__logo">
-        <img src="https://preview.colorlib.com/theme/agenda/images/logo.png" alt="" />
-      </div>
-  
-      <div className="navbar__left">    
-       <ul>
-        <li><Link id='link' to="">Ana sayfa</Link></li>
-        <li><Link  id='link' to="about">Hakkında</Link></li>
-        <li><Link id='link'  to="events">Hizmetler</Link></li>
-        <li><Link id='link'  to="news">Projeler</Link></li>
-        <li><Link  id='link' to="contact">İletişim</Link></li>
-      </ul>
-      <div className="navbar__left__icon">
-        <Link to="login"> <button><PersonIcon/></button></Link>
-    <Link to="favorite"><button><FavoriteBorderIcon/></button></Link> 
-<Link to="basket"><button><ShoppingCartIcon/></button></Link>
-      </div>
-       
+      <div className={`navbar ${isMenuOpen ? 'open' : ''}`}>
+        <div className="navbar__logo">
+          <img src="https://preview.colorlib.com/theme/agenda/images/logo.png" alt="" />
+        </div>
+
+        <div className={`navbar__left ${isMenuOpen ? 'open' : ''}`}>
+          <ul>
+            <li><Link id='link' to="">Ana sayfa</Link></li>
+            <li><Link id='link' to="about">Hakkında</Link></li>
+            <li><Link id='link' to="events">Hizmetler</Link></li>
+            <li><Link id='link' to="news">Projeler</Link></li>
+            <li><Link id='link' to="contact">İletişim</Link></li>
+          </ul>
+          <div className="navbar__left__icon">
+            <Link to="login"><button><PersonIcon /></button></Link>
+            <Link to="favorite"><button><FavoriteBorderIcon /></button></Link>
+            <Link to="basket"><button><ShoppingCartIcon /></button></Link>
+          </div>
+        </div>
+
+        <div className="navbar__left__hamburger">
+          <button id="menuicon" onClick={toggleMenu}><MenuIcon /></button>
+        </div>
       </div>
 
-<div className="navbar__left__hamburger">
-<MenuIcon id="menuicon"/>
-</div>
+     {/* Kapatma düğmesi */}
+{isMenuOpen && (
+  <button className="close-button" onClick={toggleMenu}>
+    <CloseIcon />
+  </button>
+)}
 
-    </div>
-     
+{/* Yandan açılan menü */}
+{isMenuOpen && (
+  <div className={`right-menu ${isMenuOpen ? 'open' : ''}`}>
+    <ul>
+      <li><Link to=""><PersonIcon /> Ana sayfa</Link></li>
+      <li><Link to="about"><FavoriteBorderIcon /> Hakkında</Link></li>
+      <li><Link to="events"><ShoppingCartIcon /> Hizmetler</Link></li>
+      <li><Link to="news"><MenuIcon /> Projeler</Link></li>
+      <li><Link to="contact"><MenuIcon /> İletişim</Link></li>
+    </ul>
+  </div>
+)}
+
+
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- frontend/src/pages/Site/Home/Home.jsx
+++ frontend/src/pages/Site/Home/Home.jsx
@@ -1,22 +1,54 @@
-import React from 'react'
-import Card from '../../../component/Site/Card/Card'
-import "../Home/Home.scss"
-import Slider from '../../../component/Site/Slider/Slider'
-import Hero1 from '../../../component/Site/Hero1/Hero1'
-import Carousel from '../../../component/Site/Carousel/Carousel'
-import Hero2 from '../../../component/Site/Hero2/Hero2'
+import React, { useEffect, useState } from 'react';
+import Card from '../../../component/Site/Card/Card';
+import "../Home/Home.scss";
+import Slider from '../../../component/Site/Slider/Slider';
+import Hero1 from '../../../component/Site/Hero1/Hero1';
+import Carousel from '../../../component/Site/Carousel/Carousel';
+import Hero2 from '../../../component/Site/Hero2/Hero2';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
 const Home = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <>
-    <Hero1/>  
-   
-    <Slider/>
-    <Card/>
-    <Hero2/>
- <Carousel/>
-
+      <div className="scrollable-content">
+        <Hero1/>  
+        <Slider/>
+        <Card/>
+        <Hero2/>
+        <Carousel/>
+      </div>
+      {showButton && (
+        <button className="scroll-to-top-button" onClick={scrollToTop}>
+          <KeyboardArrowUpIcon fontSize="large" />
+        </button>
+      )}
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
